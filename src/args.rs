0210b273a3c7c21125a4471d46e631a501a2f39e
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2023-01-21 12:51:01 +0100

    fix: Migrate clap 4 attributes

--- src/args.rs
+++ src/args.rs
@@ -1,20 +1,19 @@
-use clap::Parser;
+use clap::{arg, command, Parser};
 
 pub fn parse() -> Args {
     Args::parse()
 }
 
 #[derive(Debug, Parser)]
-#[clap(author, version, about)]
+#[command(author, version, about)]
 pub struct Args {
     /// URL of the target page, containing Vimeo showcase embeds
-    #[clap(action)]
     pub url: String,
 
     /// UI refresh interval in milliseconds
-    #[clap(short, long, default_value_t = 50, action)]
+    #[arg(short, long, default_value_t = 50)]
     pub tick: u64,
 
-    #[clap(flatten)]
+    #[command(flatten)]
     pub verbosity: clap_verbosity_flag::Verbosity,
 }

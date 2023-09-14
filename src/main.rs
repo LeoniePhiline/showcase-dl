1ae6ea190714ab18b5f8d4c0c42fbd30576225f2
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2023-09-14 19:26:54 +0200

    fix: Do not release terminal when creating non-fatal eyre::Report

--- src/main.rs
+++ src/main.rs
@@ -314,13 +314,16 @@ fn color_eyre_install() -> Result<()> {
     let (panic_hook, eyre_hook) = color_eyre::config::HookBuilder::default().into_hooks();
 
     // Replace `eyre_hook.install()?`.
+    //
+    // This hook is called whenever a new eyre report is generated.
+    //
+    // We do not want to release the terminal here, as we also generate eyre reports when a child process is terminated.
+    // In this case, a single download is set to 'failed' state, while the rest of the application keeps running.
+    //
+    // Instead, to print clean eyre report backtraces, we first release the terminal,
+    // then propagate any error that might have been returned from the work future to the termination of `fn main()`.
     let eyre_hook = eyre_hook.into_eyre_hook();
-    color_eyre::eyre::set_hook(Box::new(move |e| {
-        let terminal = Ui::make_terminal().expect("make terminal for error handler");
-        Ui::release_terminal(terminal).expect("release terminal for error handler");
-
-        eyre_hook(e)
-    }))?;
+    color_eyre::eyre::set_hook(Box::new(move |e| eyre_hook(e)))?;
 
     // Replace `panic_hook.install()`.
     let panic_hook = panic_hook.into_panic_hook();
